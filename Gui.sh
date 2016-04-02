fe05336e8c90f49c193a753ff92e55e03cb83b4e
Yash Sanghavi <ysanghavi1@gmail.com>
2016-04-02 19:19:01 -0400

    added some flags

--- Gui.sh
+++ Gui.sh
@@ -3,17 +3,20 @@
 CD_APP="/Applications/CocoaDialog.app"
 CD="$CD_APP/Contents/MacOS/CocoaDialog"
 
+
 $CD bubble --title "Message Opener" --text "Launching Application" --x-placement "center" --y-placement "center"
 
 cd ~/Library/Messages/Archive
 
 dates=`$CD standard-inputbox --title "Message Opener" --informative-text "Enter a date in year-month-date format" --text "Example: 2015-09-05" --float | tail -n +2`
+if [ ! -d "$dates" ]; then
+  exit
+fi
 cd "$dates"
 
 for files in *; do
 	allMes+="$files"$'\n'
 done
-
 
 rv=`$CD ok-msgbox --text "Messages from Date:" \
 --informative-text "$allMes" \
